8c8d10e9e10102fec67e60aeb6bc7b5e5655e4a9
salman alwan <salmanalwan41@gmail.com>
2019-07-02 15:20:17 +0200

    First Commit

---
+++ commands/close.js
@@ -0,0 +1,27 @@
+module.exports.run = async(client, message, args) => {
+
+    if (!message.channel.name.startsWith(`ticket-`)) return message.reply(`\nYou can't use the close command outside of a ticket channel.`);
+
+    message.reply(`\nAre you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === 'confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+        return;
+    });    
+
+
+};
+module.exports.configs = {
+    command : "close"
+};
